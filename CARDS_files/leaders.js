9143893e5536e1ca9f01ae1cae26ba0736b536a8
windowmarker <windowmarkerggs@gmail.com>
2023-01-30 09:01:39 -0500

    Leaders_test_2

--- CARDS_files/leaders.js
+++ CARDS_files/leaders.js
@@ -3,7 +3,7 @@ async function getHolders() {
     const response = await fetch('https://xchain.io/api/holders/SHARPS');
     const holdersData = await response.json();
     console.log(holdersData);
-    let leaders = '<h1>SHARPS Leaderboard</h1><table><tr><th>Rank</th><th>Address</th><th>Balance</th></tr>';
+    let leaders = '<h1>SHARPS Leaderboard</h1><table><tr><th>Estimated Value</th><th>Address</th><th>Balance</th></tr>';
     for (let i = 0; i < holdersData.length; i++) {
       leaders += `<tr><td>${i + 1}</td><td>${holdersData[i].address}</td><td>${holdersData[i].balance}</td></tr>`;
     }
