b542b46dbddab802570e8a9ca970f1b4ee6c92a3
windowmarker <windowmarkerggs@gmail.com>
2023-01-30 07:47:11 -0500

    require_test

--- leaders.js
+++ leaders.js
@@ -1,26 +1,16 @@
-const https = require('https');
-
-function getHolders() {
-https.get('https://xchain.io/api/asset/holders/SHARPS', (res) => {
-res.setEncoding('utf8');
-let rawData = '';
-res.on('data', (chunk) => { rawData += chunk; });
-res.on('end', () => {
-try {
-const holders = JSON.parse(rawData);
-let leaderboard = '<h1>SHARPS Leaderboard</h1><table><tr><th>Rank</th><th>Address</th><th>Balance</th></tr>';
-holders.forEach((holder, index) => {
-leaderboard += <tr><td>${index + 1}</td><td>${holder.address}</td><td>${holder.balance}</td></tr>;
-});
-leaderboard += '</table>';
-console.log(leaderboard);
-} catch (error) {
-console.error(error);
+async function getHolders() {
+  try {
+    const response = await fetch('https://xchain.io/api/asset/holders/SHARPS');
+    const holders = await response.json();
+    let leaderboard = '<h1>SHARPS Leaderboard</h1><table><tr><th>Rank</th><th>Address</th><th>Balance</th></tr>';
+    holders.forEach((holder, index) => {
+      leaderboard += `<tr><td>${index + 1}</td><td>${holder.address}</td><td>${holder.balance}</td></tr>`;
+    });
+    leaderboard += '</table>';
+    console.log(leaderboard);
+  } catch (error) {
+    console.error(error);
+  }
 }
-});
-}).on('error', (error) => {
-console.error(error);
-});
-}
-
+getHolders();
 setInterval(getHolders, 24 * 60 * 60 * 1000);
