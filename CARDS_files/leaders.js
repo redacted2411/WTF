8fe001cea994ed324227361cca18e80540941279
windowmarker <windowmarkerggs@gmail.com>
2023-01-30 08:40:29 -0500

    fix js

--- CARDS_files/leaders.js
+++ CARDS_files/leaders.js
@@ -1,13 +1,14 @@
 async function getHolders() {
   try {
     const response = await fetch('https://xchain.io/api/holders/SHARPS');
-    const holders = await response.json();
-    let leaderboard = '<h1>SHARPS Leaderboard</h1><table><tr><th>Rank</th><th>Address</th><th>Balance</th></tr>';
-    holders.forEach((holder, index) => {
-      leaderboard += `<tr><td>${index + 1}</td><td>${holder.address}</td><td>${holder.balance}</td></tr>`;
-    });
-    leaderboard += '</table>';
-    console.log(leaderboard);
+    const holdersData = await response.json();
+    console.log(holdersData);
+    let leaders = '<h1>SHARPS Leaderboard</h1><table><tr><th>Rank</th><th>Address</th><th>Balance</th></tr>';
+    for (let i = 0; i < holdersData.length; i++) {
+      leaders += `<tr><td>${i + 1}</td><td>${holdersData[i].address}</td><td>${holdersData[i].balance}</td></tr>`;
+    }
+    leaders += '</table>';
+    console.log(leaders);
   } catch (error) {
     console.error(error);
   }
